f6ed6f50095594faf18df7e904db4c2562f13468
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-28 14:14:32 +0900

    ログイン時に取得した情報を表示（名前、プロフィール画像）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
 
   private
 
   def logged_in?
     session[:user_id].present?
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
   end
 end
